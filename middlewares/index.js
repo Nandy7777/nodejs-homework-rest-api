9fd20a6377bb786930b5b9dfe8f9885b2e33c253
Nandy7777 <nandy7777@ukr.net>
2023-01-31 11:10:15 +0200

    something for start

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,8 @@ const { HttpError } = require('../helpers/index');
 const { Unauthorized } = require('http-errors');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
+const multer = require('multer');
+const path = require('path');
 
 function validateBody(schema) {
   return (req, res, next) => {
@@ -41,7 +43,21 @@ async function auth(req, res, next) {
   next();
 };
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, '../tmp'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
 module.exports = {
   validateBody,
   auth,
+  upload,
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { tryCatchWrapper } = require('../../helpers/index');
 const { current } = require('../../controllers/user/user');
-const {  auth } = require('../../middlewares/index');
+const { uploadImage } = require('../../controllers/user/uploadImg');
+const {  auth, upload } = require('../../middlewares/index');
 const userRouter = express.Router();
 
 userRouter.get('/current', tryCatchWrapper(auth), tryCatchWrapper(current));
-
+userRouter.patch('/avatars', upload.single('avatar'), tryCatchWrapper(uploadImage));
 module.exports = userRouter;
