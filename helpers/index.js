084c70d15489747c35a82b1a7f96c2e7daa8bba9
Nandy7777 <nandy7777@ukr.net>
2023-02-05 17:29:47 +0200

    start

--- helpers/index.js
+++ helpers/index.js
@@ -1,3 +1,6 @@
+const { EMAIL_USER, EMAIL_PASS } = process.env;
+const nodemailer = require('nodemailer');
+
 function tryCatchWrapper(enpointFn) {
   return async (req, res, next) => {
     try {
@@ -14,7 +17,28 @@ function HttpError(status, message) {
   return err;
 }
 
+async function sendMail({ to, html, subject }) {
+  const email = {
+    from: 'info@mymovies.com',
+    to,
+    subject,
+    html,
+  };
+
+  const transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: EMAIL_USER,
+      pass: EMAIL_PASS,
+    },
+  });
+
+  await transport.sendMail(email);
+}
+
 module.exports = {
   tryCatchWrapper,
   HttpError,
+  sendMail,
 };
